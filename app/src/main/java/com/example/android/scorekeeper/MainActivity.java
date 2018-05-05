d16a64ea48f1acea2f4367b5b6191802ac7f4794
Elizabeth <36939377+dizzy-miss-lizzy@users.noreply.github.com>
2018-05-05 12:32:21 -0700

    feat: Change to unspecified football score keeper, add handling of screen rotation

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.widget.TextView;
 
 /**
- * This app keeps score of the 2018 Superbowl game.
+ * This app keeps score of an American Football game.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int scorePatriots = 0;
-    int scoreEagles = 0;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    String TeamAScore;
+    String TeamBScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,94 +23,113 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Increase the score for the Patriots by 6 points.
+     * Saves team score data when screen is rotated.
      */
-    public void addSixForPatriots(View view) {
-        scorePatriots = scorePatriots + 6;
-        displayForPatriots(scorePatriots);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("TeamAScore", scoreTeamA);
+        outState.putInt("TeamBScore", scoreTeamB);
+    }
+
+    /*
+     * Restores team score data after screen rotation.
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        scoreTeamA = savedInstanceState.getInt("TeamAScore");
+        scoreTeamB = savedInstanceState.getInt("TeamBScore");
     }
 
     /**
-     * Increase the score for Patriots by 3 points.
+     * Increase the score for Team A by 6 points.
      */
-    public void addThreeForPatriots(View view) {
-        scorePatriots = scorePatriots + 3;
-        displayForPatriots(scorePatriots);
+    public void addSixForTeamA(View view) {
+        scoreTeamA += 6;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Increase the score for Patriots by 2 points.
+     * Increase the score for Team A by 3 points.
      */
-    public void addTwoForPatriots(View view) {
-        scorePatriots = scorePatriots + 2;
-        displayForPatriots(scorePatriots);
+    public void addThreeForTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Increase the score for Patriots by 1 point.
+     * Increase the score for Team A by 2 points.
      */
-    public void addOneForPatriots(View view) {
-        scorePatriots = scorePatriots + 1;
-        displayForPatriots(scorePatriots);
+    public void addTwoForTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Displays the given score for Patriots.
+     * Increase the score for Team A by 1 point.
      */
-    public void displayForPatriots(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.patriots_score);
+    public void addOneForTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
 
     /**
-     * Increase the score for the Eagles by 6 points.
+     * Increase the score for Team B by 6 points.
      */
-    public void addSixForEagles(View view) {
-        scoreEagles = scoreEagles + 6;
-        displayForEagles(scoreEagles);
+    public void addSixForTeamB(View view) {
+        scoreTeamB += 6;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
-     * Increase the score for Eagles by 3 points.
+     * Increase the score for Team B by 3 points.
      */
-    public void addThreeForEagles(View view) {
-        scoreEagles = scoreEagles + 3;
-        displayForEagles(scoreEagles);
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
-     * Increase the score for Eagles by 2 points.
+     * Increase the score for Team B by 2 points.
      */
-    public void addTwoForEagles(View view) {
-        scoreEagles = scoreEagles + 2;
-        displayForEagles(scoreEagles);
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
-     * Increase the score for Eagles by 1 point.
+     * Increase the score for Team B by 1 point.
      */
-    public void addOneForEagles(View view) {
-        scoreEagles = scoreEagles + 1;
-        displayForEagles(scoreEagles);
+    public void addOneForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
-     * Displays the given score for Eagles.
+     * Displays the given score for Team B.
      */
-    public void displayForEagles(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.eagles_score);
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
     /**
      * Resets the score for both teams back to 0.
      */
-    public void resetScore (View view) {
-        scorePatriots = 0;
-        scoreEagles = 0;
-        displayForPatriots(scorePatriots);
-        displayForEagles(scoreEagles);
+    public void resetScore(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
-
 }
